f124384f5930292bd0d6b3f8dea39112424c7672
محمد جمال احمد احمد الشبلي <98805351+Mo-Alshebli@users.noreply.github.com>
2024-10-05 02:29:25 +0300

    Update init_data.py
    update the file to localhost

--- init_data.py
+++ init_data.py
@@ -16,7 +16,7 @@ class MongoDBManager:
 
 def main():
     # Connect to MongoDB and select the Alrazi_db database
-    db_manager = MongoDBManager("mongodb://:28017/", "QB_db")
+    db_manager = MongoDBManager("mongodb://localhost:28017/", "QB_db")
 
 
     # Document for the settings_QasmiBank collection
